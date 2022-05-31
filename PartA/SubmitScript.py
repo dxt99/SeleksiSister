966957915574067581ce2dd9a7817e5ff8554e02
dxt99 <ericlouis19x@gmail.com>
2022-05-31 13:48:57 +0800

    fix: submit otp

--- PartA/SubmitScript.py
+++ PartA/SubmitScript.py
@@ -1,39 +1,27 @@
 import hashlib
 import hmac
-import math
 import time
 import requests
-import json
-from base64 import b64encode
+from base64 import b32decode, b64encode,  b32encode
+import base64
+import struct
 
-def dynamic_truncation(raw_key: hmac.HMAC, length: int) -> str:
-    bitstring = bin(int(raw_key.hexdigest(), base=16))
-    last_four_bits = bitstring[-4:]
-    # >> 0011
-    offset = int(last_four_bits, base=2)
-    # >> 3
-    chosen_32_bits = bitstring[offset * 8 : offset * 8 + 32]
-    # >> 01000100011001000111110010101110
-    full_totp = str(int(chosen_32_bits, base=2))
-    # >> 1147436206
-    return full_totp[-length:]
-    # >> 436206
+def hotp(key, counter, digits=8, digest='sha256'):
+    key = base64.b32decode(key.upper() + '=' * ((8 - len(key)) % 8))
+    counter = struct.pack('>Q', counter)
+    mac = hmac.new(key, counter, digest).digest()
+    offset = mac[-1] & 0x0f
+    binary = struct.unpack('>L', mac[offset:offset+4])[0] & 0x7fffffff
+    return str(binary)[-digits:].zfill(digits)
 
 
-def generate_totp(shared_key: str = "seleksister22" + "13520163", length: int = 8):
-    now_in_seconds = math.floor(time.time())
-    step_in_seconds = 30
-    t = math.floor(now_in_seconds / step_in_seconds)
-    hash = hmac.new(
-        bytes(shared_key, encoding="utf-8"),
-        t.to_bytes(length=8, byteorder="big"),
-        hashlib.sha256,
-    )
-
-    return dynamic_truncation(hash, length)
+def totp(key, time_step=30, digits=8, digest='sha256'):
+    return hotp(key, int(time.time() / time_step), digits, digest)
 
 if __name__ == '__main__':
     action = int(input("checksum/kumpul? (0/1): "))
+    secret = 'seleksister2213520163'
+    secret = b32encode(bytearray(secret,'ascii')).decode('utf-8')
     if(action == 0):
         #shasum
         inputFile = "partA/13520163.pdf"
@@ -43,25 +31,28 @@ if __name__ == '__main__':
         shaHashed = shaHash.hexdigest()
         openedFile.close()
         print("First 5 SHAsum: "+ shaHashed[:5])
+        secret2 = '12345678901234567890'
+        secret2 = b32encode(bytearray(secret2,'ascii')).decode('utf-8')
+        print(totp(secret2))
     else:
         #data
         body = {
                 "fullName" : "Frederik Imanuel Louis",
-                "Link"     : "https://drive.google.com/drive/folders/1_x30uA5XYF1EtwvSddlnAGJwzBJ-DgTV?usp=sharing",
-                "Message"  : "https://myanimelist.net/manga/55215/Utsuro_no_Hako_to_Zero_no_Maria"
+                "link"     : "https://drive.google.com/drive/folders/1_x30uA5XYF1EtwvSddlnAGJwzBJ-DgTV?usp=sharing",
+                "message"  : "https://myanimelist.net/manga/55215/Utsuro_no_Hako_to_Zero_no_Maria"
         }
         headers = {
             "Content-Type" : "application/json",
             "Authorization": "Basic ",
+            "Content-Length": "211"
         }
 
-        
         #otp
         userid = "13520163"
-        otp = (generate_totp())
+        otp = (totp(secret))
         authToken = b64encode(bytes(userid+":"+otp, 'utf-8')).decode('ascii')
         headers["Authorization"] += authToken
-
+        print(headers["Authorization"])
         #request
         url = "http://svr.suggoi.fun:42069/submit/a"
         req = requests.post(url, json=body, headers=headers)
